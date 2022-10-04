9f5d0e38c3b342444a75f319f561fd18f79f7306
Isuru Chandrasiri <isuru.chadrasiri@austal.com>
2022-10-04 17:01:33 +0530

    icon changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./amp.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          width={800}
+          style={{ maxHeight: 120 }}
+        />
+        <p>This is amplify test.</p>
       </header>
     </div>
   );
